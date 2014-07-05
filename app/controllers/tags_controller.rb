5e3c8d154687b123d6192aaf7251ed10a1d1a776
Harris Effron <harris.effron@gmail.com>
2014-07-05 10:55:15 -0400

    add tags controller, view, and top ten

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,23 @@
+class TagsController < ApplicationController
+  def index
+    @tags = Tag.top_ten
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.hashtag(tag_params)
+
+    if @tag.save
+      redirect_to tags_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:content)
+  end
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,20 @@
 class Tag < ActiveRecord::Base
+
+  def self.top_ten
+    Tag.all.order(:votes).limit(10).reverse_order
+  end
+
+  def self.hashtag(tag_params)
+    content = tag_params[:content]
+    existing = Tag.find_by content: content
+
+    if existing.present?
+      existing.votes += 1
+      existing
+    else
+      tag = Tag.new(tag_params)
+      tag.votes = 1
+      tag
+    end
+  end
 end
