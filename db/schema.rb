a799f5416312a0d79fcd9933e3b884cdf5cd0c9e
Harris Effron <harris.effron@gmail.com>
2014-07-06 17:18:41 -0400

    fix styles, autofocus input

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140705142537) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tags", force: true do |t|
     t.string   "content"
     t.integer  "votes"
